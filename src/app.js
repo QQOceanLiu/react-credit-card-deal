898310187ae7c840fee18b081e01fdbeabbd0b08
newOceanLiu <aijiajia0611@gmail.com>
2018-01-23 22:29:30 -0800

    initial commit, skeleton template with webpack and react

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const jsx = (
+    <h1>Save a lot of money with credit cards!</h1>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
+
